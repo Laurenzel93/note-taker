48cfa19cadd5d9094e8801e8bee70626af54bda0
Lauren Wenzel <Laurenzel93@aol.com>
2021-06-13 20:07:14 -0400

    more bug fixing

--- db/db.js
+++ db/db.js
@@ -12,13 +12,11 @@ addNote(note) {
     const { title, text } = note;
 
     if (!title || !text) {
-        throw new Error("Note 'title' and 'text' cannot be blank");
+        throw new Error("Note 'title' and 'text' cannot be empty");
     }
 
-    // Adds a unique id to the note using uuid package
     const newNote = { title, text, id: uuidv1() };
 
-    // Get all notes, add the new note, write all the updated notes, return the newNote
     return this.getNotes()
         .then((notes) => [...notes, newNote])
         .then((updatedNotes) => this.write(updatedNotes))
